ed20b0f56331d287fe2cd872c83bd97079e8cbe8
root <root@site.buksu.edu.ph>
2020-07-12 14:08:05 +0800

    Updated caldera forms overrides hooks

--- overrides/caldera-form-hooks.php
+++ overrides/caldera-form-hooks.php
@@ -28,10 +28,12 @@ function copy_requirements($src, $dst)
 
 add_filter('caldera_forms_private_upload_directory', function ($directory, $field_id, $form_id, $transient_id) {
     do_action('gw_validate_login', false);
-    // Get current user
-    $user_data = apply_filters('gw_current_user_login', null);
 
-    $directory = 'temp/'. $user_data->{'ID'} .'';
+    $user_login = apply_filters('gw_session_login_validate', function($raw){
+      return $raw;
+    });
+
+    $directory = "temp/{$user_login["utyp"]}/{$user_login["uid"]}";
 
     return $directory;
 }, 10, 4);
@@ -43,17 +45,20 @@ add_filter('caldera_forms_pre_render_form', function ($html, $entry_id, $form) {
     //change to your form ID here!
     if ('CF5ef930501b83d' == $form[ 'ID' ]) {
         $result = apply_filters('gw_validate_submitted_information', null);
+        $user_type = do_shortcode('[gw_current_user field="type"]');
+    	$email = $user_obj['uobj']['EMAIL_ADDRESS'] = do_shortcode('[gw_current_user field="email_address"]');
         $remaining_slots = do_shortcode('[gw_current_course field="slots_number_available"]');
         if ($result) {
             //echo do_shortcode("[gw_applied_course field=\'course\']");
-            return GWUtility::_gw_render_shortcode('<div class="caldera-grid"><div class="alert alert-success">
+            return GWUtility::_gw_render_shortcode("<div class=\"caldera-grid\"><div class=\"alert alert-success\">
 				        Your information has been successfully submitted. An enrollment officer will contact you through the phone number you provided.
-				            [elementor-template id="627"]
-				            [elementor-template id="617"]
-				            </div></div>');
+				            [elementor-template id=\"627\"]
+				            [elementor-template id=\"617\"]
+				            </div></div>
+                            <div style=\"text-align:center\">Your transaction request has been sent through ({$email}) for your email copy.</div>");
         }
 
-        if ($remaining_slots < 1) {
+        if ($remaining_slots < 1 && $user_type == "new") {
             return '<div class="caldera-grid"><div class="alert alert-error">We are sorry there is no slots remaining. Please choose another course</div></div>';
         }
     }
@@ -68,6 +73,7 @@ add_action('caldera_forms_submit_start', function (array $form, $process_id) {
         $raw_data = Caldera_Forms::get_submission_data($form);
         $course_field_id = Caldera_Forms_Field_Util::get_field_by_slug('course', $form)['ID'];
         $selected_course = $raw_data[$course_field_id];
+        $user_type = do_shortcode('[gw_current_user field="type"]');
         do_action('gw_validate_course_availability', $selected_course); // Do course availability validation
         $remaining_slots = do_shortcode('[gw_current_course field="slots_number_available"]');
 
@@ -75,7 +81,7 @@ add_action('caldera_forms_submit_start', function (array $form, $process_id) {
             echo "<div class=\"alert alert-error\" >Sorry, you are only allowed to submit once. Please contact (admin@mail.com) for more info.</div>";
             die();
         }
-        if ($remaining_slots < 1) {
+        if ($remaining_slots < 1 && $user_type == "new") {
             echo "<div class=\"alert alert-error\" >We are sorry there is no slots remaining. Please choose another course</div>";
             die();
         }
@@ -89,12 +95,11 @@ add_filter('caldera_forms_ajax_return', function ($out, $form) {
         $entry_id = $out['data']['cf_id'];
         $entry = new Caldera_Forms_Entry($form, $entry_id);
 
-        // Get current user
-        $user_data = apply_filters('gw_current_user_login', null);
-
-        // Get to modify fields
-        $field_uid = $entry->get_field(Caldera_Forms_Field_Util::get_field_by_slug('uid', $form)['ID']);
-        $field_examinee_number = $entry->get_field(Caldera_Forms_Field_Util::get_field_by_slug('examinee_number', $form)['ID']);
+        $user_obj = apply_filters('gw_session_login_validate', function($raw){
+          return $raw;
+        });
+        $user_id = $user_obj["uid"];
+        $user_type = $user_obj["utyp"];
 
         $raw_data = Caldera_Forms::get_submission_data($form);
 
@@ -102,8 +107,17 @@ add_filter('caldera_forms_ajax_return', function ($out, $form) {
         $selected_course = $raw_data[$course_field_id];
 
         $wp_upload_dir = wp_get_upload_dir()['basedir'];
-        $temp_directory = $wp_upload_dir . '/temp/'. $user_data->{'ID'} .'';
-        $new_directory = $wp_upload_dir . '/user-requirements/'. $user_data->{'ID'} .'';
+
+        $temp_directory = "{$wp_upload_dir}/temp/{$user_type}/{$user_id}";
+        $new_directory = "{$wp_upload_dir}/user-requirements/{$user_type}/{$user_id}";
+
+    	if (!file_exists($temp_directory)) {
+            mkdir($temp_directory, 0755, true);
+        }
+    
+        if (!file_exists($new_directory)) {
+            mkdir($new_directory, 0755, true);
+        }
 
         copy_requirements($temp_directory, $new_directory);
 
@@ -112,8 +126,22 @@ add_filter('caldera_forms_ajax_return', function ($out, $form) {
         $course_id = apply_filters('gw_get_course_meta', $selected_course, 'get_the_ID', null);
 
         $entry_manager = new GWEntriesManager(null);
-        $result = $entry_manager->request_course($user_data->{'ID'}, $course_id, json_encode($requirements_files));
+        $result = $entry_manager->request_course($user_id, $course_id, json_encode($requirements_files), $user_type);
+    
+    	$content = GWUtility::_gw_render_shortcode('<div class="caldera-grid"><div class="alert alert-success">
+				        Your information has been successfully submitted. An enrollment officer will contact you through the phone number you provided.
+				            [elementor-template id="627"]
+				            [elementor-template id="617"]
+				            </div></div>');
+    
+    	$user_obj['uobj']['EMAIL_ADDRESS'] = do_shortcode('[gw_current_user field="email_address"]');
+    
+    	// Mailer Service
+    	$mailer = new GWMailerService();
+    	$mailer->sendRequestStatus($user_obj['uobj'], $content);
     }
+
+	$out['html'] .= "<div style=\"text-align:center\">Your transaction request has been sent through ({$user_obj['uobj']['EMAIL_ADDRESS']}) for your email copy.</div>";
 
     // Render shortcode
     $out['html'] = GWUtility::_gw_render_shortcode($out['html']);
